5d1967f91e648ae8c5497ed7420fd4703ab15003
shampioner <929350123@qq.com>
2014-11-18 14:59:37 +0800

    socket com

---
+++ server.py
@@ -0,0 +1,28 @@
+import socket
+import threading,time
+def tcplink(sock,addr):
+    print 'accept new connection from %s:%s...'%addr
+    sock.send('welcome!')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if data=='exit' or not data:
+            break
+        sock.send('hello,%s!'%data)
+    sock.close()
+    print 'connection from %s:%s closed.'%addr
+
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+s.bind(('127.0.0.1',9999))
+
+s.listen(5)
+print 'waiting for connection...'
+
+while True:
+    
+    sock,addr = s.accept()
+    t = threading.Thread(target=tcplink,args=(sock,addr))
+    t.start()
+
